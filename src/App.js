72ef122f91bf4a9bd86fb0bd034192e057801ae7
nhanquoc-it <caphuunhanquoc2510@gmail.com>
2025-04-20 12:30:32 +0700

    3. Create components Header & TableUser

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.scss";
+import Header from "./components/Header";
+import TableUser from "./components/TableUser";
 
 function App() {
-	return <div>Hello world</div>;
+	return (
+		<div className="app-container">
+			<Header />
+			<TableUser />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/TableUser.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TableUser = (props) => {
+	return (
+		<>
+			<p>TableUser....</p>
+		</>
+	);
+};
+
+export default TableUser;
